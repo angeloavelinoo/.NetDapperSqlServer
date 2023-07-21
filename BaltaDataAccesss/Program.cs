ec0cfc519c1a4288a9687533f84e320d19ec7be1
Angelo Avelino <angeloavelino33211781@gmail.com>
2023-07-20 22:18:57 -0300

    Testando o Dapper

---
+++ BaltaDataAccesss/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using BaltaDataAccesss.Models;
+
+namespace BaltaDataAccesss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=DESKTOP-JOFC75D;Database=balta;Integrated Security=SSPI ";
+
+
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+
+
+
+                // CreateCategory(connection);
+                // UpdateCategory(connection);
+                // DeleteCategory(connection);
+                // ListCategories(connection);
+                GetCategory(connection);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<CategoryModel>("SELECT [Id], [Title] FROM [Category]");
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+
+        static void GetCategory(SqlConnection connection)
+        {
+            var category = connection
+                .QueryFirstOrDefault<CategoryModel>(
+                    "SELECT TOP 1 [Id], [Title] FROM [Category] WHERE [Id]=@id",
+                    new
+                    {
+                        id = "af3407aa-11ae-4621-a2ef-2028b85507c4"
+                    });
+            Console.WriteLine($"{category.Id} - {category.Title}");
+
+        }
+
+        static void CreateCategory(SqlConnection connection)
+        {
+            var category = new CategoryModel();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO 
+                    [Category] 
+                VALUES(
+                    @Id, 
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured)";
+
+            var rows = connection.Execute(insertSql, new
+            {
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+            Console.WriteLine($"{rows} linhas inseridas");
+        }
+
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = "UPDATE [Category] SET [Title]=@title WHERE [Id]=@id";
+            var rows = connection.Execute(updateQuery, new
+            {
+                id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+                title = "Frontend 2021"
+            });
+
+            Console.WriteLine($"{rows} registros atualizadas");
+        }
+
+        static void DeleteCategory(SqlConnection connection)
+        {
+            var deleteQuery = "DELETE [Category] WHERE [Id]=@id";
+            var rows = connection.Execute(deleteQuery, new
+            {
+                id = new Guid("ea8059a2-e679-4e74-99b5-e4f0b310fe6f"),
+            });
+
+            Console.WriteLine($"{rows} registros excluídos");
+        }
+    }
+}
